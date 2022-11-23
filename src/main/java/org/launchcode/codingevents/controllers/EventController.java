e8d59d300d0772be5f9cc81233c0e63fadf1970c
Chance <chance.schafer.dev@gmail.com>
2022-11-23 15:09:48 -0600

    working ORM exercises

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -25,7 +25,7 @@ public class EventController {
     public String displayAllEvents(Model model) {
         model.addAttribute("title", "All Events");
         model.addAttribute("events", eventRepository.findAll());
-        return "events/index";
+        return "index";
     }
 
     @GetMapping("create")
